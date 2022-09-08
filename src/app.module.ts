9a52a75aa211efbc21ce653dd322207a806a30e8
laribright <laribright8@gmail.com>
2022-09-08 03:37:39 +0100

    create users resource / reports resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
