f964ea3ec1849b957af573159ff439186c455eac
laribright <laribright8@gmail.com>
2022-09-09 23:46:32 +0100

    Complete users crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Query,
+  Param,
+  Delete,
+} from '@nestjs/common';
 
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -10,5 +20,25 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return this.userService.remove(Number(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(Number(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,6 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
-import { create } from 'domain';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +11,32 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    const user = this.repo.findOne({ where: { id } });
+    return user;
+  }
+
+  find(email: string) {
+    const user = this.repo.find({ where: { email } });
+    return user;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.repo.remove(user);
   }
 }
